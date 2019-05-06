d893775e816611b389debb6965cf4070ff036aee
fabian.riascos <fabian.riascos@ddbcol.com.co>
2019-05-06 14:26:11 -0500

    First commit

---
+++ src/components/Artist.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="artist" >
+    <h4><a :href="artist.url" target="_blank">{{ artist.name }}</a></h4>
+    <img :src="artist.image[2]['#text']" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Artist',
+  props: ['artist']
+}
+</script>
+
+<style scoped>
+.artist {
+    display: block;
+}
+</style>
